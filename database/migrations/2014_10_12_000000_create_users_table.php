ee9654c11c4815f404176362b7538586300f9906
vagrant <vagrant@homestead>
2021-05-06 10:11:51 +0000

    Configure Login and Register for Admin and Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->email('email')->unique();
             $table->string('password');
+            $table->integer('usertype')->default(3);
             $table->rememberToken();
             $table->timestamps();
         });
